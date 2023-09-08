8ba7004a3cae7e6b948bbfcd7d668ab70aff4181
Joao Silva <joaosilva@Joaos-MBP.lan>
2023-09-08 18:24:36 +0100

    First routes in fastify

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
-import { createServer } from "node:http";
+// import { createServer } from "node:http";
 
-const server = createServer((request, response) => {
-	response.write("Hello, World!");
+// const server = createServer((request, response) => {
+// 	response.write("Hello, World!");
 
-	response.end();
+// 	response.end();
+// });
+
+// server.listen(3333);
+
+import { fastify } from "fastify";
+
+const server = fastify();
+
+server.get("/videos", (request, response) => {
+	return "GET videos";
 });
 
-server.listen(3333);
+server.post("/videos", (request, response) => {
+	return "POST videos";
+});
+
+server.put("/videos/:id", (request, response) => {
+	return "PUT videos:id";
+});
+
+server.delete("/videos/:id", (request, response) => {
+	return "DELETE videos:id";
+});
+
+server.listen({
+	port: 3333
+});
